3ca60ea8d498a1c3d64e19c63c9e8c8bcc4aecd5
김수원 <rla5233@naver.com>
2024-06-04 15:11:54 +0900

    김수원 : 업데이트

--- Source/KSW_Unreal_Project/SideScroll/SideScrollCharacter.cpp
+++ Source/KSW_Unreal_Project/SideScroll/SideScrollCharacter.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "SideScrollCharacter.h"
+#include "SideScroll/SideScrollCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 ASideScrollCharacter::ASideScrollCharacter()
@@ -9,6 +12,20 @@ ASideScrollCharacter::ASideScrollCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 언리얼은 CDO 때문에 중간에 컴포넌트를 변경할 수 없다.
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetProjectionMode(ECameraProjectionMode::Orthographic);
+	CameraComponent->OrthoWidth = 1280.0f * 2;
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	// 언리얼에서는 회전에서 벡터를 사용하지 않고
+	// Rotator 또는 Quat 을 사용한다.
+	// 오일러 혹은 Degree 각도이다.
+	FQuat Rot = UKismetMathLibrary::Quat_MakeFromEuler(FVector(0.0f, 0.0f, -90.0f));
+	SpringArmComponent->SetRelativeRotation(Rot);
 }
 
 // Called when the game starts or when spawned
